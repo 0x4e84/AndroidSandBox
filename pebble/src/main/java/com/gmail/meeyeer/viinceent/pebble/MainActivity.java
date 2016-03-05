de803e6fb5723b5f3e5ef7965fecbdee346837af
11vmeyer <vincent.meyer@sonova.com>
2016-03-05 15:45:11 +0100

    Errors in Pebble app

--- pebble/src/main/java/com/gmail/meeyeer/viinceent/pebble/MainActivity.java
+++ pebble/src/main/java/com/gmail/meeyeer/viinceent/pebble/MainActivity.java
@@ -7,11 +7,9 @@ import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.text.method.CharacterPickerDialog;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,8 +32,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
