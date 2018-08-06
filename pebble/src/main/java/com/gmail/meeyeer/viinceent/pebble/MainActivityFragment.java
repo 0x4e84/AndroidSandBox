2374ae73f5bfa4c4ef1946f967165197efcbb18c
0x4e84 <viinceent.meeyeer@gmail.com>
2018-08-06 15:05:36 +0200

    Cleaning up Java code in the Pebble module and unregister the receiver in the onPause method to avoid leaking memory.

--- pebble/src/main/java/com/gmail/meeyeer/viinceent/pebble/MainActivityFragment.java
+++ pebble/src/main/java/com/gmail/meeyeer/viinceent/pebble/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.gmail.meeyeer.viinceent.pebble;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,7 +13,8 @@ public class MainActivityFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
